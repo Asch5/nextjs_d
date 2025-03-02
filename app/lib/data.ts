2fed73e64108bcee92371b9015a58a182636260e
Asch5 <9106543@gmail.com>
2025-03-02 22:46:29 +0100

    final

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,12 +20,12 @@ export async function fetchRevenue() {
         // Artificially delay a response for demo purposes.
         // Don't do this in production :)
 
-        console.log('Fetching revenue data...');
-        await new Promise((resolve) => setTimeout(resolve, 3000));
+        // console.log('Fetching revenue data...');
+        // await new Promise((resolve) => setTimeout(resolve, 3000));
 
         const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-        console.log('Data fetch completed after 3 seconds.');
+        // console.log('Data fetch completed after 3 seconds.');
 
         return data;
     } catch (error) {
@@ -36,6 +36,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
     try {
+        //await new Promise((resolve) => setTimeout(resolve, 2000));
         const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -60,6 +61,7 @@ export async function fetchCardData() {
         // However, we are intentionally splitting them to demonstrate
         // how to initialize multiple queries in parallel with JS.
         const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+        //await new Promise((resolve) => setTimeout(resolve, 1000));
         const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
         const invoiceStatusPromise = sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
@@ -163,7 +165,7 @@ export async function fetchInvoiceById(id: string) {
             // Convert amount from cents to dollars
             amount: invoice.amount / 100,
         }));
-
+        console.log(invoice);
         return invoice[0];
     } catch (error) {
         console.error('Database Error:', error);
